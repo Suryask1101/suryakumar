cd2c42e75987edafa3cd1fdc5485def0810dd520
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 15:03:34 +0530

    Update Skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -25,7 +25,7 @@ function Skills() {
                 <div className="skill--scroll">
                     <Marquee 
                         gradient={false} 
-                        speed={120} 
+                        speed={170} 
                         pauseOnHover={true}
                         pauseOnClick={true} 
                         delay={0}
