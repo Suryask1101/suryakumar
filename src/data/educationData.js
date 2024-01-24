8869294cdf97374be106e039850efc07a8f942f3
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 15:47:30 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,14 +1,14 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Prema Matriculation School',
+        institution: 'Prema Matriculation School,Tirupur',
         course: 'Higher Secondary Education',
         startYear: '2017',
         endYear: '2019'
     },
     {
         id: 2,
-        institution: 'Dr.N.G.P.Arts And Science College',
+        institution: 'Dr.N.G.P.Arts And Science College,Coimbatore',
         course: 'BSC - Computer Technology',
         startYear: '2020',
         endYear: '2023'
