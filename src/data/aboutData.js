b26e997b132ee7a460d7e0972ff2a2622ab2d935
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 22:10:51 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,16 +2,16 @@ export const aboutData = {
     title: "About Me",
     description1: "Hello, I'm Surya, a passionate Cloud and DevOps Engineer rooted in Bengaluru, Karnataka.",
     description2: `
-Professional Background:
-My journey in the tech realm started with a solid foundation in Computer Technology. To further enhance my skills, I pursued certifications in Linux, AWS, and DevOps. Recently, I successfully completed an advanced Cloud Computing course at Besant Technologies, Bengaluru.
+Professional Background:<br>
+My journey in the tech realm started with a solid foundation in Computer Technology. To further enhance my skills, I pursued certifications in Linux, AWS, and DevOps. Recently, I successfully completed an advanced Cloud Computing course at Besant Technologies, Bengaluru.<br><br>
 
-Expertise:
-I specialize in crafting robust cloud infrastructures, leveraging my skills in Linux, AWS, and DevOps methodologies. My goal is to contribute my expertise to dynamic and challenging projects within the tech industry.
+Expertise:<br>
+I specialize in crafting robust cloud infrastructures, leveraging my skills in Linux, AWS, and DevOps methodologies. My goal is to contribute my expertise to dynamic and challenging projects within the tech industry.<br><br>
 
-Continuous Learning:
-In the ever-evolving landscape of technology, I am committed to continuous learning. Staying updated with the latest advancements is not just a professional obligation but a personal drive that fuels my enthusiasm for innovation.
+Continuous Learning:<br>
+In the ever-evolving landscape of technology, I am committed to continuous learning. Staying updated with the latest advancements is not just a professional obligation but a personal drive that fuels my enthusiasm for innovation.<br><br>
 
-Seeking Opportunities:
+Seeking Opportunities:<br>
 Eager to explore new horizons, I am actively seeking opportunities to apply my skills and contribute to projects that push the boundaries of what technology can achieve.
     `,
     image: 2
