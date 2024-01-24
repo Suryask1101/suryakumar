7dbc2c90c256a9977cebe6ebb7e668bb8c20ccea
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 14:50:03 +0530

    Update testimonialsData.js

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import girl1 from '../assets/svg/testimonials/girl1.svg'
+/*import girl1 from '../assets/svg/testimonials/girl1.svg'
 import girl2 from '../assets/svg/testimonials/girl2.svg'
 import girl3 from '../assets/svg/testimonials/girl3.svg'
 import boy1 from '../assets/svg/testimonials/boy1.svg'
