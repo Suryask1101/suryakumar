b129958a51b89405f753f3a1790f2936a27d57f4
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 22:06:34 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,17 +2,17 @@ export const aboutData = {
     title: "About Me",
     description1: "Hello, I'm Surya, a passionate Cloud and DevOps Engineer rooted in Bengaluru, Karnataka.",
     description2: `
-        <p><strong>Professional Background:</strong></p>
-        <p>My journey in the tech realm started with a solid foundation in Computer Technology. To further enhance my skills, I pursued certifications in Linux, AWS, and DevOps. Recently, I successfully completed an advanced Cloud Computing course at Besant Technologies, Bengaluru.</p>
+Professional Background:
+My journey in the tech realm started with a solid foundation in Computer Technology. To further enhance my skills, I pursued certifications in Linux, AWS, and DevOps. Recently, I successfully completed an advanced Cloud Computing course at Besant Technologies, Bengaluru.
 
-        <p><strong>Expertise:</strong></p>
-        <p>I specialize in crafting robust cloud infrastructures, leveraging my skills in Linux, AWS, and DevOps methodologies. My goal is to contribute my expertise to dynamic and challenging projects within the tech industry.</p>
+Expertise:
+I specialize in crafting robust cloud infrastructures, leveraging my skills in Linux, AWS, and DevOps methodologies. My goal is to contribute my expertise to dynamic and challenging projects within the tech industry.
 
-        <p><strong>Continuous Learning:</strong></p>
-        <p>In the ever-evolving landscape of technology, I am committed to continuous learning. Staying updated with the latest advancements is not just a professional obligation but a personal drive that fuels my enthusiasm for innovation.</p>
+Continuous Learning:
+In the ever-evolving landscape of technology, I am committed to continuous learning. Staying updated with the latest advancements is not just a professional obligation but a personal drive that fuels my enthusiasm for innovation.
 
-        <p><strong>Seeking Opportunities:</strong></p>
-        <p>Eager to explore new horizons, I am actively seeking opportunities to apply my skills and contribute to projects that push the boundaries of what technology can achieve.</p>
+Seeking Opportunities:
+Eager to explore new horizons, I am actively seeking opportunities to apply my skills and contribute to projects that push the boundaries of what technology can achieve.
     `,
     image: 2
 };
