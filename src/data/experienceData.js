66c0e76bc6b94430a3a54e6b81cf647019f1b433
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-01-24 13:13:05 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Besant Technologies',
+        jobtitle: 'Cloud Computing - Linux and AWS',
+        startYear: '2023>jun',
+        endYear: 'Oct'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Besant technologies',
+        jobtitle: 'Devops',
+        startYear: '2023>Nov',
+        endYear: 'Jan<2024'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Coitor IT Tech',
+        jobtitle: 'Web development',
+        startYear: '2022>Jun',
+        endYear: 'Jul'
     },
 ]
