133e0b23f42e9e6d6a1ced528d56f1d9b3434799
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-06-06 12:31:14 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,12 +1,32 @@
 import one from '../assets/svg/projects/host-record.svg'
 import two from '../assets/svg/projects/surveillance-defense.svg'
 import three from '../assets/svg/projects/touch-click.svg'
+import eleven from '../assets/svg/projects/eleven.svg'
+import ten from '../assets/svg/projects/ten.svg'
 
 
 
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Automated ECS Task Scheduling with AWS Lambda',
+        projectDesc: 'Automated ECS service start/stop using Lambda & CloudWatch Events. Cuts costs by scheduling tasks as needed.',
+        tags: ['ECS', 'Lambda'],
+        code: 'https://github.com/Suryask1101/ECS-start-stop-using-lambda',
+        demo: 'https://medium.com/@suryask4530/how-to-schedule-ecs-stop-and-start-automatically-using-lambda-a3fa70e6e19f',
+        image: eleven
+    },
+    {
+        id: 2,
+        projectName: 'CI/CD pipeline for reactjs using github actions',
+        projectDesc: 'CI/CD pipeline with GitHub Actions automates EC2 builds & deployments for faster updates, minimal manual effort.',
+        tags: ['CI/CD', 'Github-actions'],
+        code: 'https://github.com/Suryask1101/CICD-Github-to-EC2',
+        demo: 'https://medium.com/@suryask4530/setup-ci-cd-pipeline-using-github-actions-for-reactjs-application-hosted-on-aws-ec2-8aefd114fb21',
+        image: ten
+    },
+    {
+        id: 3,
         projectName: 'Automated Web Application',
         projectDesc: 'Automated Jenkins deployment for a web app on Tomcat, reducing deployment time to 15 minutes, ensuring a consistent, error-free process.',
         tags: ['Jenkins', 'Tomcat Server'],
@@ -15,7 +35,7 @@ export const projectsData = [
         image: one
     },
     {
-        id: 2,
+        id: 4,
         projectName: 'Weapon Detection Through CCTV',
         projectDesc: 'The aim of the project is to detect weapons using a surveillance camera in real-time.',
         tags: ['PyQt5', 'OpenCV', 'Numpy'],
@@ -24,7 +44,7 @@ export const projectsData = [
         image: two
     },
     {
-        id: 3,
+        id: 5,
         projectName: 'Audio Book Using Python ',
         projectDesc: 'Designed and implemented a Python-based audio book application, combining text-to-speech synthesis for an engaging and accessible reading experience. ',
         tags: ['PyPDF2', 'pyttsx3','textblob'],
