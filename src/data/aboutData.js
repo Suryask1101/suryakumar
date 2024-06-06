1f079cdc1dee4f1fe6d12779a11227eaaef43b46
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-06-06 11:20:16 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,8 @@
 export const aboutData = {
     title: "About Me",
-    description1: "My name is Surya. I'm a Cloud and DevOps Engineer. Currently working as a cloud associate at Easy deploy, Coimbatore.",
-    description2: "I have a background in Computer Technology and have completed certifications in Linux, AWS, and DevOps. I recently completed an advanced Cloud Computing course at Besant Technologies, Bengaluru. My experience includes working on cloud infrastructure, and I am eager to explore new opportunities to contribute to challenging projects in the tech industry.",
+    description1: "My name is Surya, and I am a Cloud and DevOps Engineer. I recently completed a cloud associate internship at Easy Deploy, Coimbatore, where I gained valuable hands-on experience in cloud computing and DevOps practices.",
+    description2: "With a background in Computer Technology and certifications in Linux, AWS, and DevOps, I am well-equipped with the necessary skills to excel in cloud and DevOps roles. Additionally, I have completed an advanced Cloud Computing course at Besant Technologies, Bengaluru, further enhancing my expertise in cloud technologies.",
+    description3: "During my internship at Easy Deploy, I actively contributed to cloud infrastructure projects and honed my skills in various aspects of cloud computing and DevOps. I am now seeking new opportunities to leverage my experience and skills to tackle challenging projects in the tech industry.",
     image: 2
 }
 //My name is Surya. I'm a Cloud and DevOps Engineer based in Bengaluru, Karnataka.
