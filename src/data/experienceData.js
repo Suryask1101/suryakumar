eb77ea2fb7c4def8d09ac540de5fe8f91b8b7ed2
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-10-06 13:18:37 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,30 +1,23 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'IHX Private Limited',
+        jobtitle: 'DevOps Engineer',
+        startYear: '2024>Jul',
+        endYear: 'present'
+    },
+    {
+        id: 1,
         company: 'Easy Deploy',
         jobtitle: 'Cloud Associate Intern',
         startYear: '2024>Apr',
         endYear: 'Jun<2024'
     },
     {
-       id: 2,
+        id: 3,
         company: 'Besant Technologies',
-        jobtitle: 'DevOps Training',
-        startYear: '2023>Nov',
-        endYear: 'Jan<2024'
-    },
-    {
-         id: 3,
-        company: 'Besant Technologies',
-        jobtitle: 'AWS Training',
-        startYear: '2023>Aug',
-        endYear: 'Nov'
-    },
-    {
-        id: 4,
-        company: 'Besant Technologies',
-        jobtitle: 'Linux Training',
+        jobtitle: 'Cloud Computing Training',
         startYear: '2023>Jun',
-        endYear: 'Aug'
-    },
+        endYear: 'Jan<2024'
+    }
 ]
