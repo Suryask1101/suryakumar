1f6b3657ca32e566fca116257fea5964627fe77e
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-10-06 13:21:12 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'suryask4530@gmail.com',
     phone: '+917094046570',
-    address: 'BTM Layout, Bengaluru, karnataka - 560068 ',
+    address: 'Bengaluru, Karnataka, India',
 
     sheetAPI: 'https://sheetdb.io/api/v1/qnpazwl4v9aaj'
 }
