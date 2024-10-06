17ae14670b28f9b4817ba0c4781f265a4a434a04
Surya <125749622+Suryask1101@users.noreply.github.com>
2024-10-06 13:14:28 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/resume.pdf'
 import image from '../assets/png/IMG-20241002-WA0005~3.jpg'
 export const headerData = {
     name: 'Surya kumar',
-    title: "AWS DevOps Engineer",
+    title: "DevOps Engineer",
     desciption:'I am now eager to leverage my experience and skills to tackle challenging tech projects.',
     Img: image,
     resumePdf: resume
