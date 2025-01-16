74a9a2774e6411e2fabc68d4b4346a82d96cbc7f
Surya <125749622+Suryask1101@users.noreply.github.com>
2025-01-16 12:36:04 +0530

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,16 +1,24 @@
 export const educationData = [
-    {
+     {
         id: 1,
-        institution: 'Prema Matriculation School,Tirupur',
-        course: 'Higher Secondary Education',
-        startYear: '2017',
-        endYear: '2019'
+        institution: 'JAIN University, Banglore',
+        course: 'MCA - Computer Science with IT',
+        startYear: '2024',
+        endYear: 'present'
     },
-    {
+     {
         id: 2,
         institution: 'Dr.N.G.P.Arts And Science College,Coimbatore',
         course: 'BSC - Computer Technology',
         startYear: '2020',
         endYear: '2023'
+    }, 
+    {
+        id: 3,
+        institution: 'Prema Matriculation School,Tirupur',
+        course: 'Higher Secondary Education',
+        startYear: '2017',
+        endYear: '2019'
     },
+    
 ]
